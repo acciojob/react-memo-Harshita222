c012e7732b5ce1a5526bdac5526104aee288d293
Harshita222 <kumariharsita501@gmail.com>
2024-07-05 20:42:28 +0530

    room

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,23 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+import { useState } from "react";
+// 
+import Todos from "./Todos";
+import Counter from "./Counter";
+import ReactMemo from "./ReactMemo";
+const App = () => {
+    const [dataObj, setDataObj] = useState({
+        todoArr: [],
+        count: 0,
+        skillsArr: []
+    })
+  return (
+    <div id="main">
+        <h1>React.useMemo</h1>
+        <Todos dataObj={dataObj} setDataObj={setDataObj} /><hr />
+        <Counter dataObj={dataObj} setDataObj={setDataObj} /><hr /><hr />
+        <ReactMemo dataObj={dataObj} setDataObj={setDataObj} />
+    </div>
+  );
+};
 
+export default App;
